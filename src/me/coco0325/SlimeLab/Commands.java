a6f496965ce37e244070b6c17327677e770bf6a2
coco0325 <hihicoco0325@gmail.com>
2018-08-06 18:04:10 +0800

    some commands

--- src/me/coco0325/SlimeLab/Commands.java
+++ src/me/coco0325/SlimeLab/Commands.java
@@ -1,5 +1,18 @@
 package me.coco0325.SlimeLab;
 
-public class Commands {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 
+public class Commands extends PlayerWarp implements CommandExecutor{
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(cmd.getName().equals("bungeewarp")) {
+			
+		}
+		if(cmd.getName().equals("bwarpgui")) {
+			
+		}
+		return false;
+	}
 }
